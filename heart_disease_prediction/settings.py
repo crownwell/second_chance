3776e0bdf29628295853daefab736f4a3123db0f
deartist <joshuacronwell12@gmail.com>
2020-07-25 13:38:37 +0100

    change

--- heart_disease_prediction/settings.py
+++ heart_disease_prediction/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'qg!*c3)#-_14=4sc!)22u^vm99wq0q*6s#!#hg&qdla6f1o)_$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
